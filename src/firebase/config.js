abc109ce10012a6dd64397e7b9ccd7ecb6bf8738
NishantSingh02 <nst7372@gmail.com>
2023-05-07 16:02:36 +0530

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDK9fJ8BOo3qkVtLsbqdgK8FvMNSkLdFQM",
-  authDomain: "the-cook-abc7e.firebaseapp.com",
-  projectId: "the-cook-abc7e",
-  storageBucket: "the-cook-abc7e.appspot.com",
-  messagingSenderId: "823465353046",
-  appId: "1:823465353046:web:ba4dd6970f77e8a0b3c0f7",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 //init firebase
